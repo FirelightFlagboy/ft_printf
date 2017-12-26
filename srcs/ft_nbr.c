acde55b60e944e28d7ddfaf2234c0e56b006207b
Florian BENNETOT <fbenneto@e1r4p12.42.fr>
2017-12-26 16:27:21 +0100

    improve nbr

--- srcs/ft_nbr.c
+++ srcs/ft_nbr.c
@@ -6,7 +6,7 @@
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/23 09:35:08 by fbenneto          #+#    #+#             */
-/*   Updated: 2017/12/26 16:21:53 by fbenneto         ###   ########.fr       */
+/*   Updated: 2017/12/26 16:26:24 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,28 +20,56 @@ int		ft_fillnbr(uintmax_t n, t_flags f, int len, char isneg)
 	return (0);
 }
 
-int		ft_callnbr(intmax_t n, t_flags f, int len, char isneg)
+// int		ft_callnbr(intmax_t n, t_flags f, int len, char isneg)
+// {
+// 	uintmax_t	un;
+// 	size_t		flen;
+
+// 	un = (isneg) ? -n : n;
+// 	flen = len;
+// 	if (f.have_p)
+// 	{
+// 		f.have_null = 0;
+// 		if (f.precision > len)
+// 			flen = f.precision;
+// 	}
+// 	if (f.have_buff_size && f.have_null
+// 	&& (n == 0 || (isneg && !f.have_minus)))
+// 		f.buff_size--;
+// 	if (f.have_minus == 0 && f.have_buff_size)
+// 		ft_fillforward(f, isneg, flen);
+// 	ft_fillnbr(un, f, len, isneg);
+// 	if (f.have_minus == 1 && f.have_buff_size)
+// 		ft_fillbackward(f, isneg, flen);
+// 	return (1);
+// }
+
+int		ft_callnbr(uintmax_t n, t_flags f, int len, char isneg)
 {
-	uintmax_t	un;
 	size_t		flen;
 
-	un = (isneg) ? -n : n;
 	flen = len;
+	if (f.precision > len)
+		flen = f.precision;
 	if (f.have_p)
-	{
 		f.have_null = 0;
-		if (f.precision > len)
-			flen = f.precision;
+	if (f.buff_size)
+	{
+		if (f.have_null && (n == 0 || (isneg && !f.have_minus)))
+			f.buff_size--;
+		if (f.have_minus)
+		{
+			ft_fillnbr(n, f, len, isneg);
+			return (ft_fillbackward(f, isneg, flen));
+		}
+		else
+		{
+			ft_fillforward(f, isneg, flen);
+			return (ft_fillnbr(n, f, len, isneg));
+		}
 	}
-	if (f.have_buff_size && f.have_null
-	&& (n == 0 || (isneg && !f.have_minus)))
-		f.buff_size--;
-	if (f.have_minus == 0 && f.have_buff_size)
-		ft_fillforward(f, isneg, flen);
-	ft_fillnbr(un, f, len, isneg);
-	if (f.have_minus == 1 && f.have_buff_size)
-		ft_fillbackward(f, isneg, flen);
-	return (1);
+	else
+		return (ft_fillnbr(n, f, len, isneg));
 }
 
 int		ft_call_fillnbr(va_list *ap, t_flags f)
@@ -60,5 +88,5 @@ int		ft_call_fillnbr(va_list *ap, t_flags f)
 	if (n >= 0 && f.have_null\
 	&& (f.have_add || f.have_escape))
 		f.buff_size--;
-	return (ft_callnbr(n, f, l, neg));
+	return (ft_callnbr((neg) ? -n : n, f, l, neg));
 }
