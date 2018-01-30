64be684b853fedbbae03a6e1255284b5c016b819
Florian BENNETOT <fbenneto@e1r4p12.42.fr>
2018-01-30 15:17:58 +0100

    save

--- srcs/ft_strlen.c
+++ srcs/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/22 12:36:09 by fbenneto          #+#    #+#             */
-/*   Updated: 2017/12/23 14:40:52 by fbenneto         ###   ########.fr       */
+/*   Updated: 2018/01/30 15:17:50 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,44 +30,14 @@ int			ft_len_unicode(wchar_t n)
 	return (-1);
 }
 
-static size_t	my_ft_strlen_long\
-	(const char *str, const unsigned long int *longword_ptr)
+size_t		ft_strlen(char const *str)
 {
-	unsigned long int longword;
-	const char *cp;
-	
-	while (1)
-	{
-		longword = *longword_ptr++;
-		if (((longword - 0x01010101L) & 0x80808080L))
-		{
-			cp = (const char *) (longword_ptr - 1);
-			if (cp[0] == 0)
-				return cp - str;
-			if (cp[1] == 0)
-				return cp - str + 1;
-			if (cp[2] == 0)
-				return cp - str + 2;
-			if (cp[3] == 0)
-				return cp - str + 3;
-		}
-	}
-}
+	char const *s;
 
-size_t			my_ft_strlen (char const *str)
-{
-	const char *char_ptr;
-	const unsigned long int *longword_ptr;
-
-	char_ptr = str;
-	while ((unsigned long int)char_ptr & (sizeof(unsigned long int) - 1))
-	{
-		if (*char_ptr == '\0')
-			return char_ptr - str;
-		char_ptr++;
-	}
-	longword_ptr = (unsigned long int *) char_ptr;
-	return (my_ft_strlen_long(str, longword_ptr));
+	s = str;
+	while (*str)
+		str++;
+	return (str - s);
 }
 
 int			ft_get_finale_len(wchar_t *s, t_flags f)
