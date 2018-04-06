13201932d9cd05107f16dff7c3df6488c3382494
FirelightFlagboy <firelight.flagboy@gmail.com>
2018-04-06 11:30:50 +0200

    [modified :includes/ft_printf.h]

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,23 +6,39 @@
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/28 13:30:05 by fbenneto          #+#    #+#             */
-/*   Updated: 2018/03/28 13:35:21 by fbenneto         ###   ########.fr       */
+/*   Updated: 2018/04/06 11:29:48 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include <stdio.h>
 # include <stdarg.h>
+# include <string.h>
+# include <stdio.h>
 
 /*
-** Printf
+** Printf display
 */
-int			ft_printf(char const *s, ...);
-int			ft_eprintf(char const *s, ...);
-int			ft_dprintf(int fd, char const *s, ...);
-int			ft_fprintf(FILE *stream, char const *s, ...);
+int			ft_printf(char const *format, ...);
+int			ft_eprintf(char const *format, ...);
+int			ft_dprintf(int fd, char const *format, ...);
+int			ft_fprintf(FILE *stream, char const *format, ...);
+/*
+** Printf string
+*/
+int			ft_sprintf(char *s, char *format, ...);
+int			ft_snprintf(char *s, size_t n, char *format, ...);
+int			ft_asprintf(char **as, char *format, ...);
+/*
+** vprintf display
+*/
 int			ft_vfprintf(FILE *stream, char const *s, va_list ap);
 int			ft_vdprintf(int fd, char const *s, va_list ap);
+/*
+** vprintf string
+*/
+int			ft_vsprintf(char *s, char *format, va_list ap);
+int			ft_vsnprintf(char *s, size_t n, char *format, va_list ap);
+int			ft_vasprintf(char **as, char *format, va_list ap);
 #endif
